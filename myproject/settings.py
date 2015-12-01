db5332b80a3ae245f405cae532aa35a98ad7605c
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-01 16:47:45 +0530

    added readme and requirement.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Blog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '8000',
     }
 }
 
