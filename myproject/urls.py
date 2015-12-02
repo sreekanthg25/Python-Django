b55d47a0ee8b82ff60a342b5effb3885f1305eec
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-02 15:08:26 +0530

    modified blog,poplist,views,settings,urls file and deleted randompreview template

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('Blog.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+ {'document_root': settings.MEDIA_ROOT}),
 ]
