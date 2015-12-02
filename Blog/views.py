2d24e20c10367d8c9fb51d536d47a0b29e0bfed2
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-02 15:08:26 +0530

    modified blog,poplist,views,settings,urls file and deleted randompreview template

--- Blog/views.py
+++ Blog/views.py
@@ -4,7 +4,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'Blog/post_list.html', {'posts': posts})
+    randompost = Post.objects.order_by('?')[:1]
+    return render(request, 'Blog/post_list.html', {'posts': posts,'rp':randompost[0]})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
