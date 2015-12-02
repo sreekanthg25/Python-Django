f19b1a62b9024b0693d4c5c8764e889ee788303e
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-02 19:58:30 +0530

    updated with random set of articles addition

--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-
+import random
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    randompost = Post.objects.order_by('?')[:1]
-    return render(request, 'Blog/post_list.html', {'posts': posts,'rp':randompost[0]})
+    randompost = Post.objects.order_by('?')[:4]
+    return render(request, 'Blog/post_list.html', {'posts': posts,'rp':randompost[0],'randpost':randompost})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'Blog/post_detail.html', {'post': post})
+    randompost = Post.objects.order_by('?')[:4]
+    return render(request, 'Blog/post_detail.html', {'post': post, 'randpost':randompost})
