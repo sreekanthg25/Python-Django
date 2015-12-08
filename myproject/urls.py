ef0f91a26368bfcd3b0dfd910c973dbd99b7b4b5
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-08 22:40:51 +0530

    modified urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from myproject.views import IndexView
 import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     #index
-    #url('^.*$', IndexView.as_view(), name='index'),
+    url('^.*$', IndexView.as_view(), name='index'),
     #posts
     url(r'^posts/', include('Blog.urls')),	
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
