473d47fc749569d19bb9e9a5e5505f80a66ba641
sreekanthg25 <seeku.sreekanth@gmail.com>
2015-12-09 11:43:50 +0530

    added new files on front end

---
+++ static/src/app/services/post_services.js
@@ -0,0 +1,28 @@
+angular.module('blogs.postservice',[])
+.factory('PostService', function ($http, $q) {
+    var api_url = "/posts/";
+    return {
+        get: function (post_id) {
+            var url = api_url + post_id + "/";
+            var defer = $q.defer();
+            $http({method: 'GET', url: url}).
+                success(function (data, status, headers, config) {
+                    defer.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    defer.reject(status);
+                });
+            return defer.promise;
+        },
+        list: function () {
+            var defer = $q.defer();
+            $http({method: 'GET', url: api_url}).
+                success(function (data, status, headers, config) {
+                    defer.resolve(data);
+                }).error(function (data, status, headers, config) {
+                    defer.reject(status);
+                });
+            return defer.promise;
+        }
+    };
+});
